27cdda51a7f9d0569184ee69b3a26a9ac280c193
unknown <kinchoily@hotmail.com>
2021-09-27 18:08:13 +0100

    CourseInput.js Created state to be used for conditional inline styling of content

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,6 +5,7 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true) //use this state to style components conditionally
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
@@ -21,8 +22,17 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        {/* using Conditional (ternary) operator to output a different style depending on isValid state*/}
+        {/* inLine styles have the Highest priority!!! in CSS, will overwrite any other styles*/}
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            background: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
